97d37f519ae734292bef2f754236ab92109a7954
Holat <105239299+Holat@users.noreply.github.com>
2023-08-01 23:59:12 +0100

    mapScrn

--- app/modal.tsx
+++ app/modal.tsx
@@ -1,7 +1,4 @@
-import { StatusBar } from "expo-status-bar";
-import { Platform, StyleSheet, Text, View } from "react-native";
-// import { Link, Tabs } from "expo-router";
-// import { Pressable, useColorScheme } from "react-native";
+import { StyleSheet, View } from "react-native";
 
 export default function ModalScreen() {
   return <View style={styles.container}></View>;
@@ -23,26 +20,3 @@ const styles = StyleSheet.create({
     width: "80%",
   },
 });
-// [
-//   "expo-location",
-//   {
-//     "locationAlwaysAndWhenInUsePermission": "Allow $(PRODUCT_NAME) to use your location."
-//   }
-// ]
-
-/**
- * ,
-      "permissions": [
-        "android.permission.ACCESS_FINE_LOCATION",
-        "android.permission.ACCESS_BACKGROUND_LOCATION"
-      ]
- */
-
-/**
- *  "infoPlist": {
-        "NSLocationAlwaysAndWhenInUseUsageDescription": "We need your location to provide awesome features."
-      }
-
-    "permission": ["LOCATION", "BACKGROUND_LOCATION"],
-
- */
